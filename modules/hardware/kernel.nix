c74592664632d26d4b0ef0d703b4e3141289cd4d
Casey Link <unnamedrambler@gmail.com>
2024-03-15 19:58:03 +0100

    rpi4: allow kernel override

--- modules/hardware/kernel.nix
+++ modules/hardware/kernel.nix
@@ -8,7 +8,7 @@
 in {
   boot.kernelPackages =
     if platformType == "rpi3"
-    then lib.mkForce pkgs.linuxPackages_rpi3
+    then lib.mkOverride 1499 pkgs.linuxPackages_rpi3
     else if platformType == "rpi4"
     then lib.mkForce pkgs.linuxPackages_rpi4
     else if platformType == "rpizero2"
