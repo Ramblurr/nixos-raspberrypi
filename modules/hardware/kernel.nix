29cea98bfb6741d70875eb10f5fad568dcd1460e
Casey Link <unnamedrambler@gmail.com>
2024-03-15 20:00:51 +0100

    temporarily disable kernel setting

--- modules/hardware/kernel.nix
+++ modules/hardware/kernel.nix
@@ -6,12 +6,12 @@
 }: let
   platformType = config.raspberry-pi.hardware.platform.type;
 in {
-  boot.kernelPackages =
-    if platformType == "rpi3"
-    then lib.mkOverride 1499 pkgs.linuxPackages_rpi3
-    else if platformType == "rpi4"
-    then lib.mkForce pkgs.linuxPackages_rpi4
-    else if platformType == "rpizero2"
-    then lib.mkForce pkgs.linuxPackages
-    else pkgs.linuxPackages_latest;
+  #boot.kernelPackages =
+  #  if platformType == "rpi3"
+  #  then lib.mkOverride 1499 pkgs.linuxPackages_rpi3
+  #  else if platformType == "rpi4"
+  #  then lib.mkForce pkgs.linuxPackages_rpi4
+  #  else if platformType == "rpizero2"
+  #  then lib.mkForce pkgs.linuxPackages
+  #  else pkgs.linuxPackages_latest;
 }
